4383e9c710496e42f4dc161c2ecf88e04c1faba5
gonkov <115842637+gonkov@users.noreply.github.com>
2022-10-22 17:50:07 +0300

    не ценный коммент

--- TaskForTesters/Program.cs
+++ TaskForTesters/Program.cs
@@ -17,7 +17,7 @@ namespace TaskForTesters
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
-            // очень ценный коммент
+            // очень ценный коммент // да-да , спасибо
         }
     }
 }
