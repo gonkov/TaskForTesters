1764b67f1f0b30ef0ce3884f36689c83fbf12f1b
gonkov <gonkov@gmail.com>
2022-11-07 00:20:47 +0300

    change messageBox

--- TaskForTesters/Form1.cs
+++ TaskForTesters/Form1.cs
@@ -19,7 +19,7 @@ namespace TaskForTesters
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Hello Alex Enot!");
+            MessageBox.Show("Hello ENOT !!");
         }
     }
 }
